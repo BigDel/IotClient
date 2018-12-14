849041cf510987a57ddde3c311e6a06ce93aaa77
BigDel <44698529+BigDel@users.noreply.github.com>
2018-12-14 10:59:19 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+2018/12/05
+del
+配置文件
+"""
+
+# 参数
+options = {
+    'server_ip': '10.10.5.16',
+    'server_port': 6000,
+    'socket_ip': '10.10.1.20',
+    'socket_port': 6000,
+    'my_port': 3000
+}
