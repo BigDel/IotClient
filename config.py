c516eabf40dc03395d0ec2a799436909b1b540d7
BigDel <44698529+BigDel@users.noreply.github.com>
2018-12-17 14:44:40 +0800

    第二次备份
    第二次备份

--- config.py
+++ config.py
@@ -8,9 +8,29 @@ del
 
 # 参数
 options = {
-    'server_ip': '10.10.5.16',
-    'server_port': 6000,
+    'server_ip': 'http://127.0.0.1',
+    'server_port': 8888,
     'socket_ip': '10.10.1.20',
-    'socket_port': 6000,
+    'socket_port': 6001,
     'my_port': 3000
+}
+
+# 基站的信息
+myinfo = {
+    'MyPortNo': '75654321',
+    'MyTagNum': 30,
+    'MyFirstTag': '81654321',
+    'MyTagType': 'Infusion',  # Temperature,One-pieceColdChain,ProbeTypeColdChain
+}
+
+# 数据类型
+datatype = {
+    'BsHb': '0',  # 基站心跳
+    'TgHb': '1',  # 标签心跳
+    'Action': '2',  # action请求
+    'TgDt': '3',  # 标签数据
+    'TgEvt': '4',  # 标签事件
+    'RespGtSer': '5',  # 获取参数返回
+    'RespStSer': '6',  # 设置参数返回
+    'HrMe': '7'  # 下达用药数据
 }
